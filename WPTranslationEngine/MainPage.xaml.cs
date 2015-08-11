d5eba968f881f8997c9e64ab34e1c148b0b37256
Jeferson Santos <jefersons@ciandt.com>
2015-08-11 18:26:27 -0300

    Initial Version.

---
+++ WPTranslationEngine/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace WPTranslationEngine
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            TranslationManager.Translate(this);
+        }
+    }
+}
---
+++ WPTranslationEngine/TranslationManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace WPTranslationEngine
+{
+    public class TranslationManager
+    {
+        #region Private Attributes
+        private static ResourceLoader loader = null;
+        #endregion
+
+        #region Private Properties
+        private static ResourceLoader Loader
+        {
+            get
+            {
+                if (loader == null)
+                    loader = new ResourceLoader();
+
+                return loader;
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Translate all controls in the informed view where the tag attribute is filled with the translation constant.
+        /// </summary>
+        /// <param name="view">The Page or View to be translated.</param>
+        public static void Translate(DependencyObject view)
+        {
+            var controls = GetAllControlsWithTag(view);
+
+            TranslateControls(controls);
+        }
+        #endregion
+
+        #region Private Methods
+        private static void TranslateControls(List<object> controls)
+        {
+            foreach (var item in controls)
+            {
+                if ((item as ItemsControl) != null && (item as ItemsControl).Items != null)
+                {
+                    var control = item as ItemsControl;
+
+                    foreach (ContentControl itemContents in control.Items)
+                    {
+                        if (itemContents != null && itemContents.Tag != null)
+                        {
+                            var translation = Loader.GetString(itemContents.Tag.ToString());
+                            if (!string.IsNullOrEmpty(translation))
+                                itemContents.Content = translation;
+                        }
+                    }
+                    continue;
+                }
+
+                if ((item as ContentControl) != null && (item as ContentControl).Tag != null)
+                {
+                    var translation = Loader.GetString((item as ContentControl).Tag.ToString());
+
+                    if (!string.IsNullOrEmpty(translation))
+                        (item as ContentControl).Content = translation;
+                    continue;
+                }
+
+                if ((item as TextBlock) != null && (item as TextBlock).Tag != null)
+                {
+                    var translation = Loader.GetString((item as TextBlock).Tag.ToString());
+
+                    if (!string.IsNullOrEmpty(translation))
+                        (item as TextBlock).Text = translation;
+                    continue;
+                }
+            }
+        }
+
+        private static List<object> GetAllControlsWithTag(DependencyObject view)
+        {
+            var result = new List<object>();
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(view); i++)
+            {
+                var control = VisualTreeHelper.GetChild(view, i);
+
+                if ((control as Grid) == null && (control as StackPanel) == null)
+                    result.Add(VisualTreeHelper.GetChild(view, i));
+
+                result.AddRange(GetAllControlsWithTag(VisualTreeHelper.GetChild(view, i)));
+            }
+            return result;
+        }
+        #endregion
+    }
+}
